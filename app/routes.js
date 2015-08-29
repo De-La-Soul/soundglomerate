c565d681f89576f58f1afdc253a4ca5324820d39
Cristian Avalos <cavalos0086@gmail.com>
2015-08-29 16:15:22 -0700

    code clean up

--- app/routes.js
+++ app/routes.js
@@ -25,42 +25,6 @@ module.exports.requestHandler = function(route, req, res){
 };
 
 
-
-// {
-// 		url:'http://eventful.com',
-// 		path:'/events/search',
-// 		app_key: keys.EVENTFUL_KEY,
-// 		dataType:'json',
-// 		location:'San Diego',
-// 		data:'today',
-// 		count_only:'true'
-
-// 	}
-
-// var getData = function(req, res){
-// 	request.get('http://eventful.com/json/events/search?appkey=hvJMnwmRwCFF8G7B&q=music&location=San+Fransisco&count_only=true&date=today', function(err, response, body){
-// 		if(!err && res.statusCode === 200){
-// 			console.log(body);
-// 		}
-// 	});
-// };
-
-// var getData = function(req, res){
-	// var url1 = 'https://www.eventbriteapi.com/v3/events/search/?venue.city=Oakland&categories=&token=BJR6WC4U4G4AXYYC3TFM';
-	// var url1wo = 'https://www.eventbriteapi.com/v3/events/search/?venue.city=Oakland&categories=';
-
-	// var url2 = 'http://eventful.com/json/events/search/?q=music&location=San+Fransisco&count_only=true&date=today';
-	// var url2wo= 'http://eventful.com/json/events/search/?location=San+Fransisco&count_only=true&date=today';
-
-var urlObj = {
-	app_key:keys.EVENTFUL_KEY,
-	url:'http://eventful.com/json/',
-	path:'/events/search',
-	location:'Berkeley',
-	count_only:'True',
-	date:'Today'
-};
-
 var getData = function(req,res){
 	client.searchEvents({keywords:'music', location:'Berkeley', date:'today'}, function(err, data){
 		if(!err){
@@ -72,16 +36,6 @@ var getData = function(req,res){
 };
 
 
-// 	request
-// 	.get(urlObj)
-// 	.on('response', function(response){
-// 		// console.log(Object.keys(response));
-// 		console.log(response);
-// 	})
-// 	.on('error', function(error){
-// 		console.log('error', error);
-// 	});
-// };
 
 
 
