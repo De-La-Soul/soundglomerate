2aa1f94824012539e3ac0cc6a37bd468b3a29206
fvntr <phiven@gmail.com>
2015-08-28 19:11:33 -0700

    add server test

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 "use strict";
-<<<<<<< HEAD
-=======
 /*jslint node: true */
 
->>>>>>> b06443e8324432e72b52baee94454da976cdc014
 
-var path = require('path');
 var express = require('express');
 var app = express();
 
@@ -14,34 +10,17 @@ var https = require('https');
 
 var router = require('./app/routes.js');
 
-<<<<<<< HEAD
-var app = express();
-var apiRouter = express.Router(); 
-=======
->>>>>>> b06443e8324432e72b52baee94454da976cdc014
 
-
-<<<<<<< HEAD
-=======
 var port = process.env.PORT || 8080;
 
 
->>>>>>> b06443e8324432e72b52baee94454da976cdc014
 app.use(express.static(__dirname + '/public'));
 
-app.use('api', apiRouter);
+app.listen(port);
 
-app.listen(port, function(){
-	console.log('Listenning to port', port);
-});
+console.log('Listenning to port', port);
 
-<<<<<<< HEAD
-module.exports = app;
-=======
 router.requestHandler();
 
 exports = module.exports = app;
->>>>>>> b06443e8324432e72b52baee94454da976cdc014
-
-
 
