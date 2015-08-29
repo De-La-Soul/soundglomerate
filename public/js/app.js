62dcecfc776de671d02dfd88d0507b7d589f4a70
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2015-08-29 15:56:16 -0700

    Basic search page

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,26 @@
-(function(){
+// (function(){
 var app = angular.module('soundGlomerate', []);
 
 app.controller('NavBarController', function(){
   
 })
 
-})();
+app.controller('SearchController', ['$scope', function($scope) {
+  $scope.master = {};
+
+  $scope.update = function(search) {
+    $scope.master = angular.copy(search);
+    
+
+    console.log("Genre", search.genre);
+    console.log("Location", search.location);
+    // console.log(search.genre);
+    // console.log(date);
+  };
+
+  
+
+  
+}]);
+
+// })();
