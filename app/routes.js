194011fefa84b2bacfed7afb2c65976a0b14e1c7
Cristian Avalos <cavalos0086@gmail.com>
2015-08-29 16:45:05 -0700

    code cleanup

--- app/routes.js
+++ app/routes.js
@@ -2,12 +2,12 @@
 /*jslint node: true */
 'use strict';
 
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 // var url = require('url');
 // var async = require('async-series');
-var path = require('path');
-var request = require('request');
+// var path = require('path');
+// var request = require('request');
 
 // eventful API
 var keys = require(__dirname + '/../config.json');
@@ -16,11 +16,11 @@ var eventful = require('eventful-node');
 var client = new eventful.Client(keys.EVENTFUL_KEY);
 
 // /*ROUTING*/
-module.exports.requestHandler = function(route, req, res){
-		getEventfulData(req, res);
+module.exports.requestHandler = function(){
+		getEventfulData();
 };
 
-var getEventfulData = function(req,res){
+var getEventfulData = function(){
 	client.searchEvents({keywords:'music', location:'Berkeley', date:'today'}, function(err, data){
 		if(!err){
 		console.log(data.search.events);
