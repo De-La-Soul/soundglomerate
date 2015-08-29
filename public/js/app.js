218d264d44b26c89cb13f42691bac23f9672e9ef
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2015-08-29 16:37:02 -0700

    Initial landing/search page with bootstrap and angular and css

--- public/js/app.js
+++ public/js/app.js
@@ -8,6 +8,9 @@ app.controller('NavBarController', function(){
 app.controller('SearchController', ['$scope', function($scope) {
   $scope.master = {};
 
+  console.log('$scope', $scope);
+  console.log('this', this);
+
   $scope.update = function(search) {
     $scope.master = angular.copy(search);
     
