9ab502338adabffb1dfd2d4bd2029a532b7bd91c
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2015-09-02 15:05:01 -0700

    We can now search by location and music genre.

--- public/js/app.js
+++ public/js/app.js
@@ -25,13 +25,18 @@ app.controller('NavBarController', function($scope){
 app.controller('SearchController', ['$scope', '$http', function($scope, $http) {
   $scope.master = {};
   
-  $scope.apiCall = function(location){
+  $scope.apiCall = function(location, genre){
     
 
     $http({
       method: 'GET',
       datatype: 'JSON',
-          url: 'https://www.eventbriteapi.com/v3/events/search/?popular=on&sort_by=date&venue.city=' + location + '&venue.region=CA&token='
+          url: 'https://www.eventbriteapi.com/v3/events/search/?popular=on&sort_by=date&venue.city=' + location + '&venue.region=CA&categories=103&subcategories='+ genre +
+          '&token='
+
+          // https://www.eventbriteapi.com/v3/events/search/?popular=on&sort_by=date&venue.city=Oakland&venue.region=CA&categories=103&subcategories=3015&token=MD33DX7LJOIGAGCBYRF7
+
+
 
     }).success(function(data){
        
@@ -46,17 +51,26 @@ app.controller('SearchController', ['$scope', '$http', function($scope, $http) {
   $scope.update = function(search) {
     $scope.master = angular.copy(search);
 
-    console.log("Genre", search);
+    console.log("Genre", search.genre.replace(/\W/g, ""));
     console.log("Location", search.location);
-    console.log("This is this", this.search);
+    
   
     this.search = {};
+    console.log(apiGenreId[search.genre.replace(/\W/g, "")]);
 
-    $scope.apiCall(search.location);
+    $scope.apiCall(search.location, apiGenreId[search.genre.replace(/\W/g, "")]);
 
   };
 
-$scope.genres = {3001: "Alternative", 3002: "Blues & Jazz", 3003: "Classical", 3004: "Country", 3005: "Cultural", 3006: "EDM / Electronic", 3007: "Folk", 3008: "Hip Hop / Rap", 3009: "Indie", 3010: "Latin", 3011: "Metal", 3012: "Opera", 3013: "Pop", 3014: "R&B", 3015: "Reggae", 3016: "Religious/Spiritual", 3017: "Rock", 3018: "Top 40", 3999: "Other"}
+$scope.genres = 
+
+
+{3001: "Alternative", 3002: "BluesJazz", 3003: "Classical", 3004: "Country", 3005: "Cultural", 3006: "EDMElectronic", 3007: "Folk", 3008: "HipHopRap", 3009: "Indie", 3010: "Latin", 3011: "Metal", 3012: "Opera", 3013: "Pop", 3014: "RB", 3015: "Reggae", 3016: "ReligiousSpiritual", 3017: "Rock", 3018: "Top40", 3999: "Other"}
+
+
+var apiGenreId = {
+  'Alternative': "3001", 'BluesJazz': "3002", 'Classical': "3003", 'Country': "3004", 'Cultural': "3005", 'EDMElectronic': "3006", 'Folk': "3007", 'HipHopRap': "3008", 'Indie': "3009", 'Latin': "3010", 'Metal': "3011", 'Opera': "3012", 'Other': "3999", 'Pop': "3013", 'RB': "3014", 'Reggae': "3015",'ReligiousSpiritual': "3016", 'Rock': "3017", 'Top40': "3018"
+}
 
   
 }]);
