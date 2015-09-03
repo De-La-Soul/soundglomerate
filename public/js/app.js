4aefbd15475fdd48f1e92f46dbec1a736e13505a
Cristian Avalos <cavalos0086@gmail.com>
2015-09-03 15:49:59 -0700

    about to switch to the refactoring branch. Up to now, the file is working

--- public/js/app.js
+++ public/js/app.js
@@ -12,12 +12,7 @@ var app = angular.module('soundGlomerate', []);
 ///////////////////////////////////////
 
 // May not need this later
-app.controller('NavBarController', function($scope){
-  return $scope.obj = {
-    fun: 'not fun'
-  };
-  
-});
+
 
 app.controller('SearchController', ['$scope', '$http', function($scope, $http) {
   $scope.master = {};
@@ -34,10 +29,9 @@ app.controller('SearchController', ['$scope', '$http', function($scope, $http) {
       datatype: 'JSON',
           url: 'https://www.eventbriteapi.com/v3/events/search/?popular=on&sort_by=date&venue.city=' + location + '&venue.region=CA&categories=103'+ genre +
           '&token='
-
         }).success(function(data){
         $scope.dataAPI = data.events;
-      })
+      });
   };
 
   $scope.update = function(search) {
@@ -55,15 +49,13 @@ app.controller('SearchController', ['$scope', '$http', function($scope, $http) {
       $scope.apiCall(search.location);
   };
 
-$scope.genres = 
-
-
-{3001: "Alternative", 3002: "BluesJazz", 3003: "Classical", 3004: "Country", 3005: "Cultural", 3006: "EDMElectronic", 3007: "Folk", 3008: "HipHopRap", 3009: "Indie", 3010: "Latin", 3011: "Metal", 3012: "Opera", 3013: "Pop", 3014: "RB", 3015: "Reggae", 3016: "ReligiousSpiritual", 3017: "Rock", 3018: "Top40", 3999: "Other"}
+$scope.genres = {
+  3001: "Alternative", 3002: "BluesJazz", 3003: "Classical", 3004: "Country", 3005: "Cultural", 3006: "EDMElectronic", 3007: "Folk", 3008: "HipHopRap", 3009: "Indie", 3010: "Latin", 3011: "Metal", 3012: "Opera", 3013: "Pop", 3014: "RB", 3015: "Reggae", 3016: "ReligiousSpiritual", 3017: "Rock", 3018: "Top40", 3999: "Other"}
 
 
 var apiGenreId = {
   'Alternative': "3001", 'BluesJazz': "3002", 'Classical': "3003", 'Country': "3004", 'Cultural': "3005", 'EDMElectronic': "3006", 'Folk': "3007", 'HipHopRap': "3008", 'Indie': "3009", 'Latin': "3010", 'Metal': "3011", 'Opera': "3012", 'Other': "3999", 'Pop': "3013", 'RB': "3014", 'Reggae': "3015",'ReligiousSpiritual': "3016", 'Rock': "3017", 'Top40': "3018"
-}
+};
 
   
 }]);
@@ -71,8 +63,6 @@ var apiGenreId = {
   app.controller('EventBriteDataController', ['$scope', '$http', function($scope, $http){
     $scope.test = 'testing123';
   
-    
-    
   }]);
 
   // app.controller('EventFulDataController', ['$scope', '$http', function($scope, $http){
