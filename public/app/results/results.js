15525ba9fe026580e8f979c11a77728e508b7bd2
Cristian E. Avalos <cavalos0086@gmail.com>
2015-09-10 11:11:59 -0700

    messy but working filter. next: remove unused files and style it

--- public/app/results/results.js
+++ public/app/results/results.js
@@ -40,15 +40,28 @@ angular.module('soundGlomerate.results', ['soundGlomerate.resultsFactory', 'soun
   // Using 'this' to grab the event and send to the results view/controller
 
   /* ************************* CRISTIAN  *************************************/
-    console.log('events', $scope.apiData);
+
 
     $scope.genres = {3001: "Alternative", 3002: "BluesJazz", 3003: "Classical", 3004: "Country", 3005: "Cultural", 3006: "EDMElectronic", 3007: "Folk", 3008: "HipHopRap", 3009: "Indie", 3010: "Latin", 3011: "Metal", 3012: "Opera", 3013: "Pop", 3014: "RB", 3015: "Reggae", 3016: "ReligiousSpiritual", 3017: "Rock", 3018: "Top40", 3999: "Other"};
 
+    function swap(obj){
+      var result = {};
+      for(var key in obj){
+        result[obj[key]] = key;
+      }
+      return result;
+    }
+
+    $scope.genresSwap = swap($scope.genres);
+
+    // $scope.currentGenre = '3015';
     $scope.currentGenre = 'all';
 
-    $scope.setCurrentGenre = function(genre){
-      $scope.currentGenre = $scope.genres[genre];
+    function setCurrentGenre(genre){
+      $scope.currentGenre = $scope.genresSwap[genre];
       console.log($scope.currentGenre);
-    };    
+    }; 
+
+    $scope.setCurrentGenre = setCurrentGenre; 
 }]);
 
