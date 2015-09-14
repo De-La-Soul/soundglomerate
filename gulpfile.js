db4895d478be89960e280d64f4467b7876382c1d
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2015-09-14 15:34:39 -0700

    Updates README, Gulpfile, TEMPORARY fix for the hiiden api keys(see README)

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,15 @@ var mocha = require('gulp-mocha');
  
 
 
+gulp.task('build', shell.task([
+  'npm install -g nodemon', 'npm install', 'npm install -g karma-cli', 'cd /Desktop/soundglomerate'
+]));
+
+gulp.task('start', shell.task([
+   '/usr/bin/open -a "/Applications/Google Chrome.app" "http://localhost:8000"', 'nodemon index.js'
+
+]));
+
 gulp.task('lint', function() {
   return gulp.src(['public/js/*.js', 'app/*.js'])
     .pipe(jshint())
@@ -26,9 +35,12 @@ gulp.task('karma', shell.task([
   'karma start'
   ]));
 
-gulp.task('build', shell.task([
-  'npm install -g karma-cli'
-  ]))
+
+gulp.task('run', shell.task([
+   '/usr/bin/open -a "/Applications/Google Chrome.app" "http://localhost:8000"'
+
+]));
+
 
 gulp.task('default', function() {
   console.log('gulp is running');
