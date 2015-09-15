cac39ccd1d648fc1e4c596a4a4b17fc09c215be0
Cristian Avalos <cavalos0086@gmail.com>
2015-09-14 19:00:07 -0700

    commenting lines on index.js needed for database queries

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ app.listen(port);
 console.log('Listening on', url, ':', port);
 
 // DATABASE VARIABLES
-var pg = require('pg');
-var bodyParser = require('body-parser');
+// var pg = require('pg');
+// var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-// Connection string for our database
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/student';
+// // Connection string for our database
+// var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/student';
 
 
-app.post('/db/events', function(req, res){
-  // when using curl, data will be sent here
-  pg.connect(connectionString, function(err, client, done){
-    client.query('INSERT INTO events(id,name) values($1,$2)', [1,'cris Testing']);
-  });
-});
+// app.post('/db/events', function(req, res){
+//   // when using curl, data will be sent here
+//   pg.connect(connectionString, function(err, client, done){
+//     client.query('INSERT INTO events(id,name) values($1,$2)', [1,'cris Testing']);
+//   });
+// });
 
