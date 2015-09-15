0020892ff0edde8d54818aa560d1744b0829ce2b
Cristian E. Avalos <cavalos0086@gmail.com>
2015-09-14 17:34:11 -0700

    data loaded on database. reformat table next

--- db/database.js
+++ db/database.js
@@ -5,5 +5,29 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/st
 
 var client = new pg.Client(connectionString);
 client.connect();
-var query = client.query('CREATE TABLE events(id SERIAL PRIMARY KEY,name VARCHAR(40) not null)');
-query.on('end', function() { client.end(); });
+
+// create table where scrapped events are going to be stored. 
+var query = client.query('CREATE TABLE newEvents(id SERIAL ,name VARCHAR(240) not null)');
+
+// var testObj = [];
+// for(var i=0;i<10;i++){
+//   var temp={};
+//   temp['name'] = i+"cris";
+//   temp['id'] = i;
+//   testObj.push(temp);
+//   delete temp;
+// }
+
+// populate the table with the data scrapped
+var dataScrapped = require('./../Data/musicSites.js');
+var data = dataScrapped;
+// console.log('data',data);
+
+// for(var j=0;j<data.length;j++){
+//   client.query('INSERT INTO newEvents(id,name) values($1,$2)',[testObj[j]['id'], testObj[j]['name']]);
+// }
+
+for(var j=0;j<data.length;j++){
+  client.query('INSERT INTO newEvents(id,name) values($1,$2)',[j,data[j]['name']]);
+}
+// query.on('end',  function() { client.end(); }); 
