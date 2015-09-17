3553fb1ad417d6fccb1fc8d0ec514943920b4439
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2015-09-16 17:08:12 -0700

    Search bar autofill "typehead" complete, on to validation

--- public/app/search/search.js
+++ public/app/search/search.js
@@ -1,13 +1,8 @@
-angular.module('soundGlomerate.search', ['soundGlomerate.searchFactory'])
+angular.module('soundGlomerate.search', ['soundGlomerate.searchFactory', 'ui.bootstrap'])
 
 .controller('SearchController', ['$scope', '$state', 'Search', function ($scope, $state, Search) { // naming the controller 'SearchController', requiring the 'Seach' factory and the $scope module. 
 
-  $scope.cities = {
-    oakland: 'Oakland',
-    berkeley: 'Berkeley',
-    sanFrancisco: "San Francisco"
-
-  };
+  // $('#searchbar').typehead({source:?["Oakalnd", "San Francisco", "Berkeley"]})
   $scope.update = function(search){ // makes user input into an object
     if(search === undefined){
       alert("must enter a location");
@@ -15,6 +10,8 @@ angular.module('soundGlomerate.search', ['soundGlomerate.searchFactory'])
       $scope.search = search; 
     }
   }
+
+  $scope.cities = ['Oakland', 'Berkeley', 'San Francisco'];
 
   $scope.getEBEvents = function(){ // this function is called when the submit button is clicked
     console.log('message from $scope.getEBEvents in the search controller');
