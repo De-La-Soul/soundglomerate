4d18c5b6ece585dad9f5373175f12710ec7c635f
Moose <moose.techguy@gmail.com>
2015-10-04 03:29:47 -0700

    Updating again

--- public/apikeys.js
+++ public/apikeys.js
@@ -1,7 +1,7 @@
 angular.module('soundGlomerate.keysFactory', [])
 
 .factory('APIkeys', function(){
-  
+  // Eventbrite API key
   var apiKeysFactory = {
     eventBriteKey: 'MD33DX7LJOIGAGCBYRF7',
   }
--- public/app/app.js
+++ public/app/app.js
@@ -12,6 +12,7 @@ angular.module('soundGlomerate', [ // declare all controllers and services
   'soundGlomerate.searchFactory',
   'soundGlomerate.selectedEvent',
   'soundGlomerate.selectedEventFactory',
+  'angular-wurfl-image-tailor',
   'ui.bootstrap',
   'ui.router',
   'angularSpinners',
